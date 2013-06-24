b21bb92f19dad81e4e803d0cca571167dcc9d04a
Anthony Van de Gejuchte <anthonyvdgent@yahoo.com>
2013-06-24 02:07:30 +0200

    Cleanup (part 1)

--- Int64.js
+++ Int64.js
@@ -44,25 +44,13 @@ for (var i = 0; i < 256; i++) {
 /**
  * Constructor accepts any of the following argument types:
  *
- * new Int64(buffer[, offset=0]) - Existing Buffer with byte offset
- * new Int64(string)             - Hex string (throws if n is outside int64 range)
- * new Int64(number)             - Number (throws if n is outside int64 range)
- * new Int64(hi, lo)             - Raw bits as two 32-bit values
+ * new Int64(buffer[, offset=0[, isBigEndian=true]]) - Existing Buffer with byte offset
+ * new Int64(string[, isBigEndian=true])             - Hex string (throws if n is outside int64 range)
+ * new Int64(number)                                 - Number (throws if n is outside int64 range)
+ * new Int64(hi, lo)                                 - Raw bits as two 32-bit values
  */
-var Int64 = module.exports = function(a1, a2) {
-	this.bigEndian = arguments[2] == undefined ? true : arguments[2];
-	if (a1 instanceof Buffer) {
-    this.buffer = a1;
-    this.offset = a2 || 0;
-  } else {
-    this.buffer = this.buffer || new Buffer(8);
-    this.offset = 0;
-	if (typeof(a2) == "boolean"){
-		this.bigEndian = a2;
-		this.setValue.call(this,a1); //等价于this.setValue(a1);
-	}else
-    	this.setValue.apply(this, arguments);
-  }
+var Int64 = module.exports = function(a1, a2, a3) {
+  // xxx
 };
 
 
@@ -80,18 +68,18 @@ Int64.prototype = {
   _2scomp: function() {
     var b = this.buffer, o = this.offset, carry = 1;
     if(this.bigEndian == true){
-		for (var i = o + 7; i >= o; i--) {
-      		var v = (b[i] ^ 0xff) + carry;
-      		b[i] = v & 0xff;
-      		carry = v >> 8;
-		}
-    }else{
-		for(var i = o; i <= o + 7; i++){
-			var v = (b[i] ^ 0xff) + carry;
-			b[i] = v & 0xff;
-			carry = v >> 8;
-		}
-	}
+      for (var i = o + 7; i >= o; i--) {
+        var v = (b[i] ^ 0xff) + carry;
+        b[i] = v & 0xff;
+        carry = v >> 8;
+      }
+    } else {
+      for(var i = o; i <= o + 7; i++){
+        var v = (b[i] ^ 0xff) + carry;
+        b[i] = v & 0xff;
+        carry = v >> 8;
+      }
+    }
   },
 
   /**
@@ -107,7 +95,7 @@ Int64.prototype = {
       if (typeof(hi) == 'number') {
         // Simplify bitfield retrieval by using abs() value.  We restore sign
         // later
-		negate = hi < 0;
+    negate = hi < 0;
         hi = Math.abs(hi);
         lo = hi % VAL32;
         hi = hi / VAL32;
@@ -129,17 +117,17 @@ Int64.prototype = {
 
     // Copy bytes to buffer
     var b = this.buffer,o = this.offset;
-	if(this.bigEndian == true){
-    	for (var i = 7; i >= 0; i--) {
-      		b[o+i] = lo & 0xff;
-      		lo = i == 4 ? hi : lo >>> 8;
-		}
+  if(this.bigEndian == true){
+      for (var i = 7; i >= 0; i--) {
+          b[o+i] = lo & 0xff;
+          lo = i == 4 ? hi : lo >>> 8;
+    }
     }else{
-		for (var i = 0; i <= 7; i++){
-			b[o+i] = lo & 0xff;
-			lo = i == 3 ? hi : lo >>> 8;
-		}
-	}
+    for (var i = 0; i <= 7; i++){
+      b[o+i] = lo & 0xff;
+      lo = i == 3 ? hi : lo >>> 8;
+    }
+  }
 
     // Restore sign of passed argument
     if (negate) this._2scomp();
@@ -161,29 +149,29 @@ Int64.prototype = {
 
     // Running sum of octets, doing a 2's complement
    if(this.bigEndian == true){
-    	var negate = b[o] & 0x80, x = 0, carry = 1;
-    	for (var i = 7, m = 1; i >= 0; i--, m *= 256) {
-      	var v = b[o+i];
+      var negate = b[o] & 0x80, x = 0, carry = 1;
+      for (var i = 7, m = 1; i >= 0; i--, m *= 256) {
+        var v = b[o+i];
       // 2's complement for negative numbers
-      	if (negate) {
-        	v = (v ^ 0xff) + carry;
-        	carry = v >> 8;
-        	v = v & 0xff;
-      		}
-      	x += v * m;
-    	}
+        if (negate) {
+          v = (v ^ 0xff) + carry;
+          carry = v >> 8;
+          v = v & 0xff;
+          }
+        x += v * m;
+      }
     }else{
-    	var negate = b[o + 7] & 0x80, x = 0, carry = 1;
-    	for (var i = 0, m = 1; i <= 7; i++, m *= 256) {
-      	var v = b[o+i];
+      var negate = b[o + 7] & 0x80, x = 0, carry = 1;
+      for (var i = 0, m = 1; i <= 7; i++, m *= 256) {
+        var v = b[o+i];
       // 2's complement for negative numbers
-      	if (negate) {
-        	v = (v ^ 0xff) + carry;
-        	carry = v >> 8;
-        	v = v & 0xff;
-      		}
-      	x += v * m;
-    	}
+        if (negate) {
+          v = (v ^ 0xff) + carry;
+          carry = v >> 8;
+          v = v & 0xff;
+          }
+        x += v * m;
+      }
     }
 
     // Return Infinity if we've lost integer precision
